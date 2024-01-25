58e94d68ff48d08b8cf0e58e49f0a10b8a0f15ae
root <1187503260@qq.com>
2024-01-25 17:26:14 +0800

    新版带悬浮框功能 支持了悬浮窗的缩放功能

--- wpf-winthino/main.xaml.cs
+++ wpf-winthino/main.xaml.cs
@@ -23,5 +23,47 @@ namespace wpf_winthino
         {
             InitializeComponent();
         }
+        public int a = 1;
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+            if (a == 1)
+            {
+                bor.Height = 70;
+                bor.Width = 70;
+
+                this.Height = 70;
+                this.Width = 70;
+
+                Top = Top + 380;
+                Left = Left + 380;
+
+                a = 0;
+                return;
+            }
+            if (a == 0)
+            {
+                Top = Top - 380;
+                Left = Left - 380;
+
+                bor.Height = 450;
+                bor.Width = 450;
+
+                this.Height = 450;
+                this.Width = 450;
+
+                a = 1;
+                return;
+            }
+        }
+
+        private void mainf_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void but_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
     }
 }
